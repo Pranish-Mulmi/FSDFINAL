7939efae8ad107fec21a0a3993b6d843d92f82c5
Pranish-Mulmi <pranish.mulmi09@gmail.com>
2026-01-31 20:31:32 +0545

    Delete vehicle_rental directory

--- vehicle_rental/includes/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-<footer>
-    <small>&copy; <?php echo date('Y'); ?> Vehicle Rental</small>
-</footer>
-<!-- <script src="/vehicle_rental/assets/js/app.js"></script> -->
-<script src="../assets/js/app.js"></script>
-</body>
-</html>
